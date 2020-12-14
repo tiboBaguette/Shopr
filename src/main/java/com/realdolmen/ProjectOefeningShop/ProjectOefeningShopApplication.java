6600062fe6369a44ff5523abc5a336e928801884
Tibo Baguette <tibosid@hotmail.be>
2020-12-14 09:52:47 +0100

    first commit

---
+++ src/main/java/com/realdolmen/ProjectOefeningShop/ProjectOefeningShopApplication.java
@@ -0,0 +1,13 @@
+package com.realdolmen.ProjectOefeningShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOefeningShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOefeningShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/realdolmen/ProjectOefeningShop/ProjectOefeningShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realdolmen.ProjectOefeningShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectOefeningShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
